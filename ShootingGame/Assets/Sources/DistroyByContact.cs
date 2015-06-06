3c6cc6b20d6e1cdf0209ced938ac32ff22513dff
ki-hyun <inblue17@gmail.com>
2015-06-07 03:41:03 +0900

    shootingGame
    shootingGame

---
+++ ShootingGame/Assets/Sources/DistroyByContact.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	GameObject GameController;
+
+	void Start () 
+	{
+		GameController = GameObject.Find("GameController");
+	}
+	
+
+	void OnTriggerEnter(Collider other)
+	{	
+		if (other.gameObject.tag == "Enemy") 
+		{
+			return;
+		}
+
+		if (explosion != null) 
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+
+
+		
+
+		if (other.tag == "Player") 
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			GameController.GetComponent<GameController> ().GameOver();
+		}
+
+
+		GameController.GetComponent<GameController> ().AddScore (1);
+
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
---
+++ ShootingGame/Assets/Sources/GameController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject[] hazards;
+	public float startWait = 1;
+	public float spawnWait = 0.75f;
+	public float waveWait = 2;
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	int score;
+	bool gameover;
+	bool restart;
+
+	void Start () 
+	{
+		Screen.SetResolution (480, 800, false);
+		score = 0;
+		restartText.text = "";
+		gameOverText.text = "";
+		gameover = false;
+		restart = false;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves());
+	}
+
+	public void AddScore(int newScore)
+	{
+		score += newScore;
+		UpdateScore ();
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameover = true;
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score : " + score;
+	}
+	
+	IEnumerator	SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+
+		while (true) 
+		{
+
+			for (int i = 0; i < 10; ++i) 
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 SpawnPosition = new Vector3 (Random.Range(-5, 5), 5, 16);
+				Quaternion spawnRotation = Quaternion.Euler (new Vector3 (0, 180, 0));
+				Instantiate (hazard, SpawnPosition, spawnRotation);
+				if (gameover == true)
+				{
+					restartText.text= "Press 'R' for Restart";
+					restart = true;
+					break;
+				}
+				yield return new WaitForSeconds (spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+
+		}
+	}
+
+	void Update () 
+	{
+		if(restart == true)
+		{
+			if(Input.GetKeyDown(KeyCode.R) == true)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+}
